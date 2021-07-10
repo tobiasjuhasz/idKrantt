b3685e281cb33285c84320c9952f23a292a8a682
Tobias Juhasz <tobiasjuhasz@gmail.com>
2021-07-09 23:38:50 -0300

    updates

---
+++ configuration.py
@@ -0,0 +1,5 @@
+## This is a config file, everything here is crucial for the functionality of the application. Please see README.md before doing anything else.
+
+class __config__():
+    token = ""
+    figmap = ""
---
+++ main.py
@@ -0,0 +1,7 @@
+#  This software has been developed is property of Tobias Juhasz. 
+#  CC0 1.0 Universal License in LICENSE file.
+
+import __config__ as cfg
+
+class __main__():
+    
